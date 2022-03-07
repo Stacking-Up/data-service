72133a98b390bc2f4f940e76deb7a90dfa7cd746
alesancor1 <alesancor1@alum.us.es>
2022-03-07 17:57:44 +0100

    test: beforeAll & afterAll tests

--- tests/index.test.js
+++ tests/index.test.js
@@ -3,14 +3,19 @@
 const userTest = require('./suites/user.test');
 const spaceTest = require('./suites/space.test');
 const rentalTest = require('./suites/rental.test');
+const server = require('../server');
 
 describe('Unit testing', () => {
-    before(function() {
-        //Before all tests
-        return;
+    before( (done) => {
+        server.deploy('test').then( () => done());
     });
 
     describe('User Tests', userTest.bind(this));
     describe('Space Tests', spaceTest.bind(this));
     describe('Rental Tests', rentalTest.bind(this));
+
+    after( (done) => {
+        server.undeploy();
+        done();
+    });
 });
