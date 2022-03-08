bd79f132f712198f9b46052e362a3c9baf5d6c67
alesancor1 <alesancor1@alum.us.es>
2022-03-08 02:27:47 +0100

    test: Added first tests

--- tests/suites/user.test.js
+++ tests/suites/user.test.js
@@ -1,7 +1,45 @@
 const assert = require('assert');
+const axios = require('axios');
+const sinon = require('sinon');
 
-module.exports = () => {
-  it('should pass', function() {
-    assert.equal(true, true);
+const host = "http://localhost:4100";
+
+module.exports = (prisma) => {
+  let findMany = sinon.stub(prisma.user, 'findMany').rejects("Not implemented");
+  let findUnique = sinon.stub(prisma.user, 'findUnique').rejects("Not implemented");
+
+  before(() => {
+    sinon.replace(prisma.user, 'findMany', findMany);
+    sinon.replace(prisma.user, 'findUnique', findUnique);
+  });
+
+  /***************************************************************************
+  * USER UNIT TESTS
+  ***************************************************************************/
+  it('should return empty list when no users are found in DB', async () => {
+      // Mock DB Query
+      findMany.withArgs({}).resolves([]);
+
+      // API Call
+      await axios.get(`${host}/api/v1/users`).then(res => {
+        assert.equal(res.status, 200);
+        assert.equal(res.data.length, 0);
+      });
+  });
+
+  it('should return data in DB when present', async () => {
+    // Fixture
+    const dbOutput = [{id:1, name: 'John', surname: 'Doe', avatar: null}, {id:2, name: 'Jane', surname: 'Doe'}];
+    const expected = [{id:1, name: 'John', surname: 'Doe'}, {id:2, name: 'Jane', surname: 'Doe'}];
+    
+    // Mock DB Query
+    findMany.withArgs({}).resolves(dbOutput);
+
+    // API Call
+    await axios.get(`${host}/api/v1/users`).then(res => {
+      assert.equal(res.status, 200);
+      assert.equal(res.data.length, 2);
+      assert.deepEqual(res.data, expected);
+    });
   });
 }
