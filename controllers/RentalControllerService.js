54e865379fb4528148cd4b78a6843174ba8d0e1b
antoniofm00 <72383530+antoniofm00@users.noreply.github.com>
2022-03-08 13:32:33 +0100

    feat: Implemented GET /api/v1/rentals

--- controllers/RentalControllerService.js
+++ controllers/RentalControllerService.js
@@ -1,9 +1,17 @@
 'use strict';
 
+const utils = require('../utils');
+const prisma = require('../prisma');
+
 module.exports.getRentals = function getRentals (req, res, next) {
-  res.send({
-    message: 'This is the mockup controller for getRentals'
-  });
+  prisma.rental.findMany({})
+    .then(rentals => {
+      res.send(rentals.map(rental =>utils.excludeNulls(rental)));
+    })
+    .catch(err =>{
+      console.error(err);
+      re.status(500).send("Server error: Could not get rentals.")
+    });
 };
 
 module.exports.getRental = function getRental (req, res, next) {
