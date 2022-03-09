1f71ca0d7108f324b89a47dfeb8f5073485f0b7e
Carlos Atienza <72754340+carlosatyca@users.noreply.github.com>
2022-03-09 13:15:50 +0100

    feat: Added filter queries for GET spaces

--- controllers/SpaceControllerService.js
+++ controllers/SpaceControllerService.js
@@ -1,9 +1,54 @@
 'use strict';
 
+const utils = require('../utils');
+const prisma = require('../prisma');
+
 module.exports.getSpaces = function getSpaces (req, res, next) {
-  res.send({
-    message: 'This is the mockup controller for getSpaces'
-  });
+  // Get tags selected for tag filtering
+  const tagsFilter = req.tag.value !== undefined ? utils.parseTags(req.tag.value) : [];
+
+  // Get max and min values for dimensions filtering
+  const minDimension = req.minDim.value !== undefined ? req.minDim.value : 0;
+  const maxDimension = req.maxDim.value !== undefined ? req.maxDim.value : Number.MAX_VALUE;
+
+  // Get max and min values for priceHour filtering
+  const minPriceHour = req.minPriceHour.value !== undefined ? req.minPriceHour.value : 0;
+  const maxPriceHour = req.maxPriceHour.value !== undefined ? req.maxPriceHour.value : Number.MAX_VALUE;
+
+  // Get max and min values for priceDay filtering
+  const minPriceDay = req.minPriceDay.value !== undefined ? req.minPriceDay.value : 0;
+  const maxPriceDay = req.maxPriceDay.value !== undefined ? req.maxPriceDay.value : Number.MAX_VALUE;
+
+  // Get max and min values for priceMonth filtering
+  const minPriceMonth = req.minPriceMonth.value !== undefined ? req.minPriceMonth.value : 0;
+  const maxPriceMonth = req.maxPriceMonth.value !== undefined ? req.maxPriceMonth.value : Number.MAX_VALUE;
+
+  prisma.space.findMany({
+    take: req.limit.value,
+    skip: req.offset.value,
+    where: {
+      shared: { equals: req.shared.value }
+    },
+    include: {
+      tags: true
+    }
+  })
+    .then(spaces => {
+      res.send(spaces.filter(space =>
+        utils.inRange(minDimension, maxDimension, utils.getMeters(space.dimensions)) &&
+        utils.includesTags(tagsFilter, utils.tagsToArray(space.tags)) &&
+        utils.inRange(minPriceHour, maxPriceHour, space.priceHour) &&
+        utils.inRange(minPriceDay, maxPriceDay, space.priceDay) &&
+        utils.inRange(minPriceMonth, maxPriceMonth, space.priceMonth) &&
+        utils.isRentedPer(req.isRentPerHour.value, space.priceHour) &&
+        utils.isRentedPer(req.isRentPerDay.value, space.priceDay) &&
+        utils.isRentedPer(req.isRentPerMonth.value, space.priceMonth)
+      ).map(space => utils.notNulls(space)));
+    })
+    .catch(err => {
+      console.error(err);
+      res.status(500).send('Server error: Could not get spaces');
+    });
 };
 
 module.exports.getSpace = function getSpace (req, res, next) {
---
+++ utils/index.js
@@ -0,0 +1,60 @@
+'use-strict';
+
+module.exports.notNulls = (obj) => {
+  const res = {};
+  Object.entries(obj).forEach(([key, value]) => {
+    if (obj[key] !== null) res[key] = value;
+  });
+  return res;
+};
+
+module.exports.getMeters = (obj) => {
+  const splitted = obj.split('x');
+  return parseFloat(splitted[0]) * parseFloat(splitted[1]);
+};
+
+module.exports.parseTags = (value) => {
+  const tags = value.split(',');
+  return tags;
+};
+
+module.exports.tagsToArray = (object) => {
+  const res = [];
+  if (object !== undefined) {
+    object.map(x => res.push(x.tag));
+  }
+
+  return res;
+};
+
+module.exports.includesTags = (selectedTags, spaceTags) => {
+  let res = true;
+  if (selectedTags.length !== 0) {
+    res = selectedTags.every(tag => spaceTags.includes(tag));
+  }
+
+  return res;
+};
+
+module.exports.inRange = (min, max, value) => {
+  let res;
+  if (value === null && min === 0 && max === Number.MAX_VALUE) {
+    res = true;
+  } else if (value === null && (min !== 0 || max !== Number.MAX_VALUE)) {
+    res = false;
+  } else {
+    res = parseFloat(min) <= value && value <= parseFloat(max);
+  }
+  return res;
+};
+
+module.exports.isRentedPer = (value, price) => {
+  let res = true;
+
+  if (value === true && price === null) {
+    res = false;
+  } else if (value === false && price !== null) {
+    res = false;
+  }
+  return res;
+};
