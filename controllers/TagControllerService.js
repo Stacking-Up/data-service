713a2603219eb13783f88caa6128e72fa57476a1
tomgalbar <72520181+tomgalbar@users.noreply.github.com>
2022-03-12 17:08:32 +0100

    refactor: GET Tags Endpoint

--- controllers/TagControllerService.js
+++ controllers/TagControllerService.js
@@ -1,12 +1,11 @@
 'use strict';
 
 module.exports.getTags = function getTags (req, res, next) {
-    const tags = require('@prisma/client')?.TagEnum ? Object.values(require('@prisma/client').TagEnum) : undefined;
-
-    if(tags !== undefined) {
-        res.status(200).send(tags);
-    }
-    else {
-        res.status(500).send('Internal Server Error');
-    }
+  try {
+    const tags = Object.values(require('@prisma/client').TagEnum);
+    res.status(200).send(tags);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send('Internal Server Error');
+  }
 };
