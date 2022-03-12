ebacb5ad4f2167e8d4c5a267c1f7bf190383967d
tomgalbar <72520181+tomgalbar@users.noreply.github.com>
2022-03-12 17:08:07 +0100

    style: Lint fix

--- controllers/TagController.js
+++ controllers/TagController.js
@@ -3,5 +3,5 @@
 const varTagController = require('./TagControllerService');
 
 module.exports.getTags = function getTags (req, res, next) {
-    varTagController.getTags(req.swagger.params, res, next);
+  varTagController.getTags(req.swagger.params, res, next);
 };
