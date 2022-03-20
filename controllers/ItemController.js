631ec78308fcbe4c61075cbcf9f98a81b6c56ed0
tomgalbar <72520181+tomgalbar@users.noreply.github.com>
2022-03-21 00:32:27 +0100

    feat: Added user items post endpoint

--- controllers/ItemController.js
+++ controllers/ItemController.js
@@ -9,3 +9,7 @@ module.exports.getItemsTypes = function getItemsTypes (req, res, next) {
 module.exports.getItemsDimensions = function getItemsDimensions (req, res, next) {
   varItemController.getItemsDimensions(req.swagger.params, res, next);
 };
+
+module.exports.postItems = function postItems (req, res, next) {
+  varItemController.postItems(req, res, next);
+};
