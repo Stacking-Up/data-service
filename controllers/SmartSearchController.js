fd808b2774b3e13e02e52656799910fa1eab4d7c
alesancor1 <alesancor1@alum.us.es>
2022-03-20 02:13:23 +0100

    feat: Implemented Renter RS based on tags

--- controllers/SmartSearchController.js
+++ controllers/SmartSearchController.js
@@ -8,3 +8,6 @@ module.exports.postTrainSpaces = function postTrainSpaces (req, res, next) {
 module.exports.getSpaces = function getSpaces (req, res, next) {
   varSmartSearchControllerService.getSpaces(req, res, next);
 };
+module.exports.getRenters = function getRenters (req, res, next) {
+  varSmartSearchControllerService.getRenters(req, res, next);
+};
--- controllers/SmartSearchControllerService.js
+++ controllers/SmartSearchControllerService.js
@@ -12,38 +12,36 @@ module.exports.getSpaces = async function getSpaces (req, res, next) {
   const longitude = req.swagger.params.longitude.value;
 
   // Initial checks
-  (() => {
-    if (!items || items.length === 0) {
-      res.status(400).send('No items provided');
-      return;
-    }
+  if (!items || items.length === 0) {
+    res.status(400).send('No items provided');
+    return;
+  }
 
-    if (!items.every(item => item instanceof Object &&
-        Object.prototype.hasOwnProperty.call(items, 'type') &&
-        Object.prototype.hasOwnProperty.call(items, 'dimensions'))) {
-      res.status(400).send('Invalid item format');
-      return;
-    }
+  if (!items.every(item => item instanceof Object &&
+      Object.prototype.hasOwnProperty.call(item, 'type') &&
+      Object.prototype.hasOwnProperty.call(item, 'dimensions'))) {
+    res.status(400).send('Invalid item format');
+    return;
+  }
 
-    if (!items.map(item => item.type).every(type => Object.values(ItemType).includes(type))) {
-      res.status(400).send('Invalid item type');
-      return;
-    }
+  if (!items.map(item => item.type).every(type => Object.values(ItemType).includes(type))) {
+    res.status(400).send('Invalid item type');
+    return;
+  }
 
-    if (!items.map(item => item.dimensions).every(dim => Object.values(Dimensions).includes(dim))) {
-      res.status(400).send('Invalid item dimensions');
-      return;
-    }
+  if (!items.map(item => item.dimensions).every(dim => Object.values(Dimensions).includes(dim))) {
+    res.status(400).send('Invalid item dimensions');
+    return;
+  }
 
-    if (!latitude || !longitude) {
-      res.status(400).send('No location coords provided');
-      return;
-    }
+  if (!latitude || !longitude) {
+    res.status(400).send('No location coords provided');
+    return;
+  }
 
-    if (isNaN(latitude) || isNaN(longitude)) {
-      res.status(400).send('Invalid location coords');
-    }
-  })();
+  if (isNaN(latitude) || isNaN(longitude)) {
+    res.status(400).send('Invalid location coords');
+  }
 
   // Recomendations based on tfidf
   let recommendedSpaces = [];
@@ -149,5 +147,87 @@ module.exports.postTrainSpaces = async function postTrainSpaces (req, res, next)
     }
   } else {
     res.status(401).send('Unauthorized');
+  }
+};
+
+module.exports.getRenters = async function getSenters (req, res, next) {
+  const authToken = req.cookies?.authToken;
+  const spaceId = req.swagger.params.spaceId.value;
+
+  // Initial checks
+  if (!authToken) {
+    res.status(401).send('Unauthorized');
+    return;
+  }
+
+  if (!spaceId || !spaceId.toString().match(/^\d+$/)) {
+    res.status(400).send('Invalid space id');
+    return;
+  }
+
+  try {
+    const decoded = jwt.verify(authToken, process.env.JWT_SECRET || 'stackingupsecretlocal');
+
+    if (decoded.role === 'USER') {
+      res.status(403).send('Forbidden');
+      return;
+    }
+  } catch (err) {
+    if (err instanceof jwt.JsonWebTokenError) {
+      res.status(401).send(`Unauthorized: ${err.message}`);
+    } else {
+      console.error(err);
+      res.status(500).send('Internal Server Error');
+    }
+    return;
+  }
+
+  const space = await prisma.space.findUnique({ where: { id: spaceId }, include: { tags: true } });
+
+  if (!space || !space.tags || space.tags.length === 0) {
+    res.status(404).send('Space cannot be found or has no tags to recommend users based on');
+    return;
+  }
+
+  // Recomendations
+  try {
+    const tagsPerItemType = space.tags.reduce((acc, tag) => {
+      Object.entries(smartSearch.dictItemsTags).forEach(([itemType, itemTags]) => {
+        if (itemTags.includes(tag.tag)) acc[itemType] += 1 / itemTags.length;
+      });
+      return acc;
+    }, { APPLIANCES: 0, ELECTRONICS: 0, CLOTHES: 0, FURNITURE: 0, DIYs: 0, OTHER: 0 });
+
+    const userItemsRatio = (await prisma.user.findMany({ include: { items: true } }))
+      .filter(user => user.items && user.items.length > 0)
+      .filter(user => spaceUtils.getMeters(space.dimensions) >= user.items.reduce((acc, i) => { acc += smartSearch.dictDimensionsEnum[i.dimensions]; return acc; }, 0))
+      .reduce((acc, user) => {
+        acc[user.id] = { APPLIANCES: 0, ELECTRONICS: 0, CLOTHES: 0, FURNITURE: 0, DIYs: 0, OTHER: 0 };
+        user.items.forEach(i => {
+          acc[user.id][i.type] += 1 / user.items.length;
+        });
+        return acc;
+      }, {});
+
+    if (Object.keys(userItemsRatio).length === 0) {
+      res.status(404).send('No users found to recommend');
+    } else {
+      const recommendations = Object.entries(userItemsRatio).reduce((recs, [userId, itemsRatio]) => {
+        // Euclidean distance between userItems and spaceTags items
+        const score = 1 - Math.sqrt(
+          (itemsRatio.APPLIANCES - tagsPerItemType.APPLIANCES) ** 2 +
+          (itemsRatio.ELECTRONICS - tagsPerItemType.ELECTRONICS) ** 2 +
+          (itemsRatio.CLOTHES - tagsPerItemType.CLOTHES) ** 2 +
+          (itemsRatio.FURNITURE - tagsPerItemType.FURNITURE) ** 2 +
+          (itemsRatio.DIYs - tagsPerItemType.DIYs) ** 2 +
+          (itemsRatio.OTHER - tagsPerItemType.OTHER) ** 2);
+        recs.push({ id: parseInt(userId), score: score });
+        return recs;
+      }, []).sort((a, b) => b.score - a.score).slice(0, 15);
+      res.status(200).send(recommendations);
+    }
+  } catch (err) {
+    console.error(err);
+    res.status(500).send('Internal Server Error');
   }
 };
